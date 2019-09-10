1b8bc56bc2fdcc03616f9a9ab8c0e2dd9e130127
WarLordYong <54880199+WarLordYong@users.noreply.github.com>
2019-09-10 18:34:29 +0800

    bbs

--- WebContent/js/Gitalk.js
+++ WebContent/js/Gitalk.js
@@ -1,10 +1,11 @@
+var title = location.pathname.substr(0, 50);
 var gitalk = new Gitalk({
   clientID: 'c0e09dde74ed2bed4c7f',
-  clientSecret: 'b264e445a3e043106103d83a106529a9b55241a2',
-  repo: 'MyBlog',
+  clientSecret: 'a860d0852132a370cd0df94be84de7edb0519968',
+  repo: 'WarLordYong.github.io',
   owner: 'WarLordYong',
   admin: ['WarLordYong'],
-  id: location.pathname,      // 请确保你的 location 连接小于 50 个字符，否则，插件会生成失败
+  id: title,      // 请确保你的 location 连接小于 50 个字符，否则，插件会生成失败
   distractionFreeMode: false  // 专注模式
 })
 
