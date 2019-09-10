0fc8da23b5b01ff997746fb4d85c4ea921204657
WarLordYong <54880199+WarLordYong@users.noreply.github.com>
2019-09-10 18:03:27 +0800

    Update Gitalk.js

--- WebContent/js/Gitalk.js
+++ WebContent/js/Gitalk.js
@@ -1,7 +1,7 @@
 var gitalk = new Gitalk({
   clientID: 'c0e09dde74ed2bed4c7f',
   clientSecret: 'b264e445a3e043106103d83a106529a9b55241a2',
-  repo: 'xiangyongshuo',
+  repo: 'MyBlog',
   owner: 'WarLordYong',
   admin: ['WarLordYong'],
   id: location.pathname,      // 请确保你的 location 连接小于 50 个字符，否则，插件会生成失败
