86bec3e3667f6e3cf5a9ecf56c144a3043ba85bf
WarLordYong <54880199+WarLordYong@users.noreply.github.com>
2019-09-10 19:29:10 +0800

    bbs

--- WebContent/js/Gitalk.js
+++ WebContent/js/Gitalk.js
@@ -1,7 +1,7 @@
 var title = location.pathname.substr(0, 50);
 var gitalk = new Gitalk({
-  clientID: 'c0e09dde74ed2bed4c7f',
-  clientSecret: 'a860d0852132a370cd0df94be84de7edb0519968',
+  clientID: '6c101277004da299d026',
+  clientSecret: '43049bab185926d4704a294d5bfff97aac69297d',
   repo: 'WarLordYong.github.io',
   owner: 'WarLordYong',
   admin: ['WarLordYong'],
